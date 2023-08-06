35ecb377492c9267f3734d72a0a6eb4abeb54766
Banyc <36535895+Banyc@users.noreply.github.com>
2023-08-06 14:02:44 +0800

    style: comments

--- src/categorical.rs
+++ src/categorical.rs
@@ -5,8 +5,11 @@ pub fn one_proportion(p_hat: f64, n: usize, p_0: f64) -> f64 {
     assert!(p_hat >= 0.);
     assert!(p_0 <= 1.);
     assert!(p_0 >= 0.);
+
+    // Normality check
     assert!(p_hat * n as f64 >= 10.);
     assert!((1. - p_hat) * n as f64 >= 10.);
+
     let standard_error = ((p_0 * (1. - p_0)) / n as f64).sqrt();
     let z = (p_hat - p_0) / standard_error;
     Z_SCORE_TABLE.p_value_two_sided(z)
@@ -25,10 +28,13 @@ pub fn difference_of_two_proportions(
     assert!(p_hat_2 >= 0.);
     assert!(p_0 <= 1.);
     assert!(p_0 >= 0.);
+
+    // Normality check
     assert!(p_hat_1 * n_1 as f64 >= 10.);
     assert!((1. - p_hat_1) * n_1 as f64 >= 10.);
     assert!(p_hat_2 * n_2 as f64 >= 10.);
     assert!((1. - p_hat_2) * n_2 as f64 >= 10.);
+
     let standard_error = (((p_hat_1 * (1. - p_hat_1)) / n_1 as f64)
         + ((p_hat_2 * (1. - p_hat_2)) / n_2 as f64))
         .sqrt();
