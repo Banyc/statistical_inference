53b2a7266ea889f147ec6f1f772fa5db6cfcc1fa
Banyc <36535895+Banyc@users.noreply.github.com>
2023-08-06 14:25:45 +0800

    feat: fitness

--- src/categorical.rs
+++ src/categorical.rs
@@ -1,4 +1,6 @@
-use crate::distributions::normal::Z_SCORE_TABLE;
+use std::num::NonZeroUsize;
+
+use crate::distributions::{chi_square::CHI_SQUARE_TABLE, normal::Z_SCORE_TABLE};
 
 pub fn one_proportion(p_hat: f64, n: usize, p_0: f64) -> f64 {
     assert!(p_hat <= 1.);
@@ -42,6 +44,31 @@ pub fn difference_of_two_proportions(
     Z_SCORE_TABLE.p_value_two_sided(z)
 }
 
+#[derive(Debug, Copy, Clone)]
+pub struct CountAndExpect {
+    pub count: usize,
+    pub expect: f64,
+}
+
+/// Null hypothesis: counts from each column is equal to their expected counts respectively
+pub fn fitness(catagories: &[CountAndExpect]) -> f64 {
+    let df = NonZeroUsize::new(catagories.len() - 1).unwrap();
+
+    // Normality check
+    catagories.iter().for_each(|bin| assert!(bin.expect >= 5.));
+
+    let chi_square = catagories
+        .iter()
+        .map(|bin| {
+            let standard_error_square = bin.expect;
+            let z_square = (bin.count as f64 - bin.expect).powi(2) / standard_error_square;
+            #[allow(clippy::let_and_return)]
+            z_square
+        })
+        .sum();
+    CHI_SQUARE_TABLE.p_value(df, chi_square)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -63,5 +90,28 @@ mod tests {
             ) > 0.05
         );
         assert!(difference_of_two_proportions(0.958, 1000, 0.899, 1000, 0.03) < 0.05);
+    }
+
+    #[test]
+    fn test_fitness() {
+        let bins = [
+            CountAndExpect {
+                count: 205,
+                expect: 198.,
+            },
+            CountAndExpect {
+                count: 26,
+                expect: 19.25,
+            },
+            CountAndExpect {
+                count: 25,
+                expect: 33.,
+            },
+            CountAndExpect {
+                count: 19,
+                expect: 24.75,
+            },
+        ];
+        assert!(fitness(&bins) > 0.05);
     }
 }
