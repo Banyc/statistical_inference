56211dd601bcf90835f712e980b2600974d5caa7
Banyc <36535895+Banyc@users.noreply.github.com>
2023-08-06 17:08:22 +0800

    feat: difference_of_two_means

--- src/numerical.rs
+++ src/numerical.rs
@@ -2,10 +2,36 @@ use std::num::NonZeroUsize;
 
 use crate::distributions::t::T_SCORE_TABLE;
 
-pub fn one_sample_mean(mean: f64, standard_deviation: f64, n: usize, mean_0: f64) -> f64 {
-    let standard_error = standard_deviation / (n as f64).sqrt();
-    let t = (mean - mean_0) / standard_error;
-    let df = NonZeroUsize::new(n - 1).unwrap();
+#[derive(Debug, Clone, Copy)]
+pub struct NumericalSample {
+    pub mean: f64,
+    pub deviation: f64,
+    pub n: usize,
+}
+
+impl NumericalSample {
+    pub fn standard_error_square(&self) -> f64 {
+        self.deviation / (self.n as f64)
+    }
+}
+
+pub fn one_sample_mean(sample: NumericalSample, mean_0: f64) -> f64 {
+    let standard_error = sample.standard_error_square().sqrt();
+    let t = (sample.mean - mean_0) / standard_error;
+    let df = NonZeroUsize::new(sample.n - 1).unwrap();
+    T_SCORE_TABLE.p_value_two_sided(df, t)
+}
+
+pub fn difference_of_two_means(
+    sample_1: NumericalSample,
+    sample_2: NumericalSample,
+    mean_0: f64,
+) -> f64 {
+    let standard_error =
+        (sample_1.standard_error_square() + sample_2.standard_error_square()).sqrt();
+    let t = (sample_1.mean - sample_2.mean - mean_0) / standard_error;
+    let df = usize::min(sample_1.n, sample_2.n) - 1;
+    let df = NonZeroUsize::new(df).unwrap();
     T_SCORE_TABLE.p_value_two_sided(df, t)
 }
 
@@ -15,6 +41,34 @@ mod tests {
 
     #[test]
     fn test_one_sample_mean() {
-        assert!(one_sample_mean(97.32, 16.98, 100, 93.29) < 0.05);
+        assert!(
+            one_sample_mean(
+                NumericalSample {
+                    mean: 97.32,
+                    deviation: 16.98_f64.powi(2),
+                    n: 100,
+                },
+                93.29
+            ) < 0.05
+        );
+    }
+
+    #[test]
+    fn test_difference_of_two_means() {
+        assert!(
+            difference_of_two_means(
+                NumericalSample {
+                    mean: 7.18,
+                    deviation: 1.60_f64.powi(2),
+                    n: 100,
+                },
+                NumericalSample {
+                    mean: 6.78,
+                    deviation: 1.43_f64.powi(2),
+                    n: 50,
+                },
+                0.
+            ) >= 0.05
+        );
     }
 }
